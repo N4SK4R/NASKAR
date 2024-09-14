50333f3d14e1b6d6820395ab12078515717d3044
Aditya Naskar <115721424+N4SK4R@users.noreply.github.com>
2024-09-14 20:00:53 +0530

    Add files via upload

---
+++ boot/disk_load.asm
@@ -0,0 +1,24 @@
+disk_load:                             ; load DH sectors to ES:BX from drive DL
+    
+    push dx
+    mov ah, 0x02                       ; BIOS function to read sector
+    mov al, dh                         ; Read DH sectors
+    mov dl, [BOOT_DRIVE_NUMBER]        ; Drive Number
+    mov ch, 0x00                       ; Select cylinder 0
+    mov dh, 0x00                       ; Select head 0
+    mov cl, 0x02                       ; Start reading from sector 2
+    int 0x13
+
+    jc disk_error      ; Jump if error (carry flag set)
+
+    pop dx
+    cmp dh, al
+    jne disk_error 
+    ret
+
+disk_error :
+    mov si, DISK_ERROR_MSG
+    call print_string
+    jmp $
+
+DISK_ERROR_MSG db "Disk read error!", 0
