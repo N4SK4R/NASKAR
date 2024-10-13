abe627435d1ca3cae475d03609a8e915104f0040
Aditya Naskar <115721424+N4SK4R@users.noreply.github.com>
2024-10-13 12:37:34 +0530

    Add files via upload

--- kernel/cpu/idt.c
+++ kernel/cpu/idt.c
@@ -1,19 +1,42 @@
 #include "idt.h"
+#include "../drivers/port.h"
 
-idt_gate g_IDT[256];
+IDT_gate g_IDT[256];
 
-idt_register g_IDTDescriptor = { sizeof(g_IDT) - 1, g_IDT };
+IDT_register g_IDTDescriptor;
 
-void IDT_SetGate(int interrupt, void *base)
+
+void IDT_SetGate(int interrupt, unsigned long base)
 {
-    g_IDT[interrupt].low_offset = ((uint32_t)base) & 0xFFFF;
-    g_IDT[interrupt].segment_selector= 0x08;
+    g_IDT[interrupt].offset_lower = (base) & 0xFFFF;
+    g_IDT[interrupt].segment_selector = 0x08;
     g_IDT[interrupt].always0 = 0;
     g_IDT[interrupt].flags = 0x8E;
-    g_IDT[interrupt].high_offset = ((uint32_t)base >> 16) & 0xFFFF;
+    g_IDT[interrupt].offset_higher = (base >> 16) & 0xFFFF;
 }
 
-void idt_init()
-{
-    idt_load(&g_IDTDescriptor);
+void IDT_Initialize()
+{   
+    g_IDTDescriptor.base = (unsigned long) &g_IDT;
+    g_IDTDescriptor.limit = sizeof(g_IDT) - 1;
+    IDT_load(&g_IDTDescriptor);
+
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+
+    // ICW2
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+
+    // ICW3
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+
+    // ICW4
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+
+    // OCW1
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0);
 }
