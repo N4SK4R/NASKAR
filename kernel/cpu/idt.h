c8fbef6cb943a25d8c8b2ced40298bb81ea60778
Aditya Naskar <115721424+N4SK4R@users.noreply.github.com>
2024-10-13 12:37:15 +0530

    Add files via upload

--- kernel/cpu/idt.h
+++ kernel/cpu/idt.h
@@ -1,19 +1,21 @@
-#pragma once
-#include<stdint.h>
+#ifndef IDT_H
+#define IDT_H
 
 typedef struct {
-    uint16_t low_offset;
-    uint16_t segment_selector;   
-    uint8_t always0;
-    uint8_t flags;
-    uint16_t high_offset;
-} __attribute__((packed)) idt_gate;
+    unsigned short offset_lower;
+    unsigned short segment_selector;
+    unsigned char always0;
+    unsigned char flags;
+    unsigned short offset_higher;
+} __attribute__((packed)) IDT_gate;
 
 typedef struct {
-    uint16_t limit;
-    idt_gate* base;
-} __attribute__((packed)) idt_register;
+    unsigned short limit;
+    unsigned long  base;
+} __attribute__((packed)) IDT_register;
 
-void IDT_SetGate(int interrupt, void* base);
-void idt_init();
-void __attribute__((cdecl)) idt_load(idt_register* idtDescriptor);
+void IDT_SetGate(int interrupt, unsigned long base);
+extern void __attribute__((cdecl)) IDT_load(IDT_register* IDTDescriptor);
+void IDT_Initialize();
+
+#endif 
